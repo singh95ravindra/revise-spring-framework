dfa9debb21fe8f7db535c5c8d2534e86beece7e6
singh95ravindra <singh95ravindra@email.com>
2019-11-07 20:14:16 +0530

    accept path variable.

--- learn-spring-mvc/src/main/java/com/practice/springmvc/hello/HelloController.java
+++ learn-spring-mvc/src/main/java/com/practice/springmvc/hello/HelloController.java
@@ -1,16 +1,17 @@
 package com.practice.springmvc.hello;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld() {
+	@RequestMapping("/welcome/{name}")
+	public ModelAndView helloWorld(@PathVariable("name") String name) {
 		final ModelAndView helloView = new ModelAndView("HelloPage");
-		helloView.addObject("welcomeMessage", "Hi there, welcome to spring mvc!");
+		helloView.addObject("welcomeMessage", "welcome " + name);
 		return helloView;
 	}
 
